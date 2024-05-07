55423a8d3d8f2c896a2d5658e9c5c296845c01ec
Neals86 <84263400+Neals86@users.noreply.github.com>
2024-05-07 12:12:13 +0200

    Update debounce.h

--- debounce.h
+++ debounce.h
@@ -8,7 +8,7 @@ void init_button(uint8_t button_pin) {
     PORTB |= (1 << button_pin);
 }
 
-int is_button_pressed(uint8_t button_pin){
+int debounce(uint8_t button_pin){
     if (!(PINB & (1 << button_pin))) {
         _delay_ms(10); // Debounce delay
         if (!(PINB & (1 << button_pin))) {
