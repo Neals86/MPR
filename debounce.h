0b1a05aa68ac69d37c72c9f5ac9e548d82ab599b
Neals86 <84263400+Neals86@users.noreply.github.com>
2024-05-07 11:04:24 +0200

    Update debounce.h

--- debounce.h
+++ debounce.h
@@ -2,14 +2,13 @@
 #include <util/delay.h>
 
 void init_button(uint8_t button_pin) {
-    // Set button_pin as input
+    
     DDRB &= ~(1 << button_pin);
     // Enable pull-up resistor for button_pin
     PORTB |= (1 << button_pin);
 }
 
-int is_button_pressed(uint8_t button_pin) {
-    // Check if the button is pressed
+int is_button_pressed(uint8_t button_pin){
     if (!(PINB & (1 << button_pin))) {
         _delay_ms(10); // Debounce delay
         if (!(PINB & (1 << button_pin))) {
