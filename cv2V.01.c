10098db5f70d4cb048424887c3cf248a19be7486
Neals86 <84263400+Neals86@users.noreply.github.com>
2024-10-16 12:03:16 +0200

    Update cv2V.01.c

--- cv2V.01.c
+++ cv2V.01.c
@@ -1,16 +1,15 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define F_CPU 16000000UL  
-#define ENC_A (1 << PA0)     
-#define ENC_B (1 << PA1)     
-#define ENC_BTN (1 << PA2)   
-#define SEG_PORT PORTC     
-#define DIG_PORT PORTD     
+#define F_CPU 16000000UL
+#define ENC_A (1 << PA0)
+#define ENC_B (1 << PA1)
+#define ENC_BTN (1 << PA2)
+#define SEG_PORT PORTC
+#define DIG_PORT PORTD
 
-volatile int counter = 0;  
-volatile int last_A = 0, last_B = 0;  
-
+volatile int counter = 0;
 unsigned char digit_segments[16] = {
     0x81, 0xf3, 0x49, 0x61, 0x33, 0x25, 0x05, 0xf1,
     0x01, 0x21, 0x11, 0x07, 0x8d, 0x43, 0x0d, 0x1d
@@ -24,59 +23,59 @@ void display_number(int number) {
     digits[3] = number % 10;
 
     for (int i = 0; i < 4; i++) {
-        DIG_PORT = ~(1 << i); 
-        SEG_PORT = digit_segments[digits[i]]; 
-        _delay_ms(5); // Increased delay to 5ms 
+        DIG_PORT = ~(1 << i);
+        SEG_PORT = digit_segments[digits[i]];
+        _delay_ms(5); // Increased delay to 5ms
         SEG_PORT = 0xFF; // Turn off all segments before switching
     }
 }
 
-void poll_encoder() {
+ISR(PCINT0_vect) {
     int new_A = PINA & ENC_A;
     int new_B = PINA & ENC_B;
+    static int last_A = 0;
 
-    if ((last_A != 0) && (new_A == 0)) {
-        if (new_B != 0) {
+    if (new_A && !last_A) {
+        if (new_B) {
             if (counter < 9999) counter++;
         } else {
             if (counter > 0) counter--;
-        }
-    }
-
-    if ((last_B != 0) && (new_B == 0)) {
-        if (new_A != 0) {
-            if (counter > 0) counter--;
-        } else {
-            if (counter < 9999) counter++;
         }
     }
 
     last_A = new_A;
-    last_B = new_B;
 }
 
-void poll_reset_button() {
-    if (!(PINA & ENC_BTN)) { 
-        counter = 0; 
-        _delay_ms(200); 
+ISR(INT0_vect) {
+    if (!(PINA & ENC_BTN)) {
+        counter = 0;
+        _delay_ms(200);
     }
 }
 
 void setup() {
-    DDRA &= ~(ENC_A | ENC_B | ENC_BTN); 
+    DDRA &= ~(ENC_A | ENC_B | ENC_BTN);
     PORTA |= (ENC_A | ENC_B | ENC_BTN);
     DDRC = 0xFF;
-    DDRD = 0xFF; 
+    DDRD = 0xFF;
     SEG_PORT = 0xFF;
     DIG_PORT = 0xFF;
+
+    // Enable pin change interrupt on PA0 and PA1
+    PCICR |= (1 << PCIE0);
+    PCMSK0 |= (ENC_A | ENC_B);
+
+    // Enable external interrupt on PA2
+    EICRA |= (1 << ISC01);
+    EIMSK |= (1 << INT0);
+
+    sei(); // Enable global interrupts
 }
 
 int main(void) {
-    setup(); 
+    setup();
 
     while (1) {
-        poll_encoder(); 
-        poll_reset_button(); 
-        display_number(counter); 
+        display_number(counter);
     }
 }
